3f5dc57bf193958aba78927762df61da38fe4fe7
Biswajeet Das <me.athe1stb@gmail.com>
2022-04-27 11:56:58 +0530

    updated UI and added functionality to dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -2,11 +2,11 @@
 let f = document.querySelectorAll(".frame");
 
 for (let i = 0; i < f.length; i++) {
-  if (i % 2 == 1) {
-    f[i].classList.remove("faculty");
-    f[i].classList.add("faculty-alt");
-  }
-  console.log(f[i].classList);
+    if (i % 2 == 1) {
+        f[i].classList.remove("faculty");
+        f[i].classList.add("faculty-alt");
+    }
+    console.log(f[i].classList);
 }
 
 f = document.querySelectorAll(".dock");
@@ -66,20 +66,37 @@ let tempData = {
 // fill up
 let cards = document.querySelectorAll(".preferences");
 
+function setComment(id, totalLoad) {
+    let comment = document.getElementById(id);
+    console.log(comment);
+    if (totalLoad < 20) {
+        comment.textContent = "LOW";
+        comment.style.color = "yellow";
+    } else if (totalLoad <= 40) {
+        comment.textContent = "MEDIUM";
+        comment.style.color = "orange";
+    } else {
+        comment.textContent = "HIGH";
+        comment.style.color = "red";
+    }
+}
+
 for (let i = 0; i < cards.length; i++) {
     let totalLoad = 0;
-
-    for (let j = 0; j < 6; j++) {
+    setComment("f" + (i + 1) + "_comment", totalLoad);
+    for (let j = 0; j < 4; j++) {
         let idn = j + 1;
         let idd = "f" + (i + 1) + "_pref" + idn.toString();
         let tid = "pref" + idn.toString();
 
-        document.getElementById(idd).textContent = tempData[tid]["name"];
-        let load = tempData[tid]["L"] * a + tempData[tid]["T"] * b + tempData[tid]["P"] * c;
-        document.getElementById(idd + "load").textContent = load.toString();
+        // document.getElementById(idd).textContent = tempData[tid]["name"];
+        // let load = tempData[tid]["L"] * a + tempData[tid]["T"] * b + tempData[tid]["P"] * c;
+        let loadID = idd + "load";
+        // console.log(loadID);
+        let load = parseInt(document.getElementById(loadID).textContent);
 
         let checkbox = document.getElementById(idd + "cb");
-        console.log(checkbox);
+        // console.log(checkbox);
         let checked = checkbox.checked;
 
         checkbox.addEventListener("click", (event) => {
@@ -90,9 +107,11 @@ for (let i = 0; i < cards.length; i++) {
                 totalLoad += load;
                 checked = !checked;
             }
-            let totalID = "f" + (i+1) + "_totalLoad";
+            let totalID = "f" + (i + 1) + "_totalLoad";
             document.getElementById(totalID).textContent = totalLoad.toString();
+            let commentID = "f" + (i + 1) + "_comment";
+            setComment(commentID, totalLoad);
         });
-
     }
+
 }
