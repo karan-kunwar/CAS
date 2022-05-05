49db45fd7b8c3f8ed55b47ecf80e560cf7ce3d52
Karan Kunwar <karankunwar59@gmail.com>
2022-05-05 14:38:02 +0530

    Major UI changes and minor faculty query added

--- controllers/facultyController.js
+++ controllers/facultyController.js
@@ -1,6 +1,15 @@
 const client = require("../db.js");
 const path = require("path");
 
+async function getObj(q) {
+  client.query(q, (err, result) => {
+    if (err) throw err;
+    let fobj = result["rows"];
+    return fobj;
+  });
+  return [];
+}
+
 let facultyLoginGet = (req, res) => {
   res.render("facultyLogin");
 };
@@ -9,14 +18,14 @@ let facultyLoginPost = async (req, res) => {
   let email_id = req.body.email_id;
   let password = req.body.password;
   console.log(email_id, password); //
-//   client.query(
-//     "SELECT $1::text as message",
-//     ["Hello world bsdwalo!"],
-//     (err, res) => {
-//       console.log(err ? err.stack : res.rows[0].message); // Hello World!
-//       client.end();
-//     }
-//   );
+  //   client.query(
+  //     "SELECT $1::text as message",
+  //     ["Hello world bsdwalo!"],
+  //     (err, res) => {
+  //       console.log(err ? err.stack : res.rows[0].message); // Hello World!
+  //       client.end();
+  //     }
+  //   );
   res.redirect("facultyIndex"); // To be removed
 
   if (!(email_id && password)) return res.redirect("/faculty/login");
@@ -43,28 +52,47 @@ let facultyLoginPost = async (req, res) => {
 };
 
 let facultyIndexGet = (req, res) => {
-  // TODO: Query to get the courses
-  res.render("facultyIndex", {
-    user: "KK",
-    courses: ["ENB", "RSG", "DMW", "LAL", "BCC"],
+  // TODO: Query to get the list of courses
+  let query = `select sc.semcourseid as "SemCourseId", 
+  c.coursename as "Course", c.lecture as "L", c.tutorial as "T", 
+  c.practical as "P", bs.sem as "Semester", b.branchname as "Branch"
+  from sem_course as sc
+  inner join branch_sem as bs using(branchsemid)
+  inner join course as c using (courseid)
+  inner join branch as b using (branchid)
+  where bs.sem % 2 = 1
+  order by bs.sem asc;`;
+
+  client.query(query, (err, result) => {
+    if (err) throw err;
+    let _courses = result["rows"];
+    res.render("facultyIndex", {
+      user: "KK",
+      courses: _courses,
+    });
   });
 };
 
-let tempdata = { '1': 'BCC', '2': 'DMW', '3': 'RSG', '4': 'LAL' };
+let tempdata = [];
 
 let facultyIndexPost = (req, res) => {
-  let preference = req.body;
-  console.log(preference);
-  res.redirect("dashboard")
+  let preference = [];
+  for(let x in req.body){
+    let obj = JSON.parse(req.body[x]);
+    preference.push(obj);
+  }
+  tempdata = preference;
+  console.log(tempdata)
+  res.redirect("dashboard");
 };
 
 let facultyDashboardGet = (req, res) => {
-
-  res.render("facultyDashboard", { 
-      user : "KK",
-      courses: tempdata,
-    });
+  res.render("facultyDashboard", {
+    user: "KK",
+    courses: tempdata,
+  });
 };
+
 module.exports = {
   facultyLoginGet,
   facultyLoginPost,
